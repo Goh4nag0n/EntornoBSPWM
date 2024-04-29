bfbad5bffd559768b10382a529a20344404c8eb1
Goh4nag0n <164566003+Goh4nag0n@users.noreply.github.com>
2024-04-29 09:20:50 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -212,6 +212,11 @@ function nvimInstall (){
     sudo rm -rf nvim
     cp -r $user/.config/nvim .
     nvim
+    sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+
+    echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+    sudo apt update
+    sudo apt install brave-browser
     #dale a no 
     #si quieres quitar el $ en el nvim te metes en el archivo con nano ~/.config/nvim/lua/core/init.lua escribes la opcion vim.opt.list=false y listo.
 }
