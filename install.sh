49155b10a899247a14d9838a7ececffbe55abc5f
Goh4nag0n <164566003+Goh4nag0n@users.noreply.github.com>
2024-05-11 21:17:56 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -188,6 +188,11 @@ function utilsPermission (){
 
     echo -e "\n${yellowColour}[+]${endColour}${grayColour} Removiendo Repositorios${endColour}"
     rm -rf ~/github
+    
+    sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+    echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+    sudo apt update
+    sudo apt install brave-browser
 }
 
 function nvimInstall (){
@@ -203,20 +208,7 @@ function nvimInstall (){
     cd /opt/nvim-linux64/bin
     ./nvim
     sudo apt remove neovim
-    cd
-    user=$(pwd)
-    
-    sudo su
-    cd
-    cd ~/.config/nvim 
-    sudo rm -rf nvim
-    cp -r $user/.config/nvim .
-    nvim
-    sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
-
-    echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-    sudo apt update
-    sudo apt install brave-browser
+   
     #dale a no 
     #si quieres quitar el $ en el nvim te metes en el archivo con nano ~/.config/nvim/lua/core/init.lua escribes la opcion vim.opt.list=false y listo.
 }
